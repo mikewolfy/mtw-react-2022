f4592fa129abe90bec5dbae98a3dba636a720144
Michael T Wolfgang <149036@carmax.com>
2022-12-01 14:15:11 -0500

    Got stocks working

--- my-app/src/StockComponent.js
+++ my-app/src/StockComponent.js
@@ -12,25 +12,25 @@ class StockComponent extends React.Component {
     }
   
     componentDidMount() {
-      fetch("https://mtw-investing-v2-api.azurewebsites.net/api/stocks")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              items: result
-            });
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
+      fetch("https://mtw-investing-v2-api.azurewebsites.net/api/stocks", {})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
     }
   
     render() {
@@ -41,13 +41,30 @@ class StockComponent extends React.Component {
         return <div>Loading...</div>;
       } else {
         return (
-          <ul>
-            {items.map(item => (
-              <li key={item.ticker}>
-                {item.ticker} {item.price}
-              </li>
-            ))}
-          </ul>
+          <table class="table">
+            <thead>
+              <tr>
+                  <th>Ticker</th>
+                  <th>Name</th>
+                  <th>Sector</th>
+                  <th>Price</th>
+                  <th>PE</th>
+                  <th>Daily Change</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map(item => (
+                <tr>
+                  <td>{item.ticker}</td>
+                  <td>{item.name}</td>
+                  <td>{item.industry}</td>
+                  <td>{item.price}</td>
+                  <td>{item.metrics.pe}</td>
+                  <td>{item.dailyPercentChange}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         );
       }
     }
