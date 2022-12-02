5ea23d67c2dc781037d779fdf6a3a24909ccbafc
Michael T Wolfgang <149036@carmax.com>
2022-12-02 16:07:44 -0500

    Update StockComponent.js

--- my-app/src/StockComponent.js
+++ my-app/src/StockComponent.js
@@ -38,7 +38,7 @@ class StockComponent extends React.Component {
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
-        return <div>Loading...</div>;
+        return <div align="center"><h2>Loading...</h2></div>;
       } else {
         return (
           <div align="center">
