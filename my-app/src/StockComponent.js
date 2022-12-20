f263f3c4ec58aa66d01671f7e0327d63a1b1edbb
Michael T Wolfgang <149036@carmax.com>
2022-12-20 17:06:50 -0500

    Added sorting

--- my-app/src/StockComponent.js
+++ my-app/src/StockComponent.js
@@ -8,7 +8,10 @@ class StockComponent extends React.Component {
       this.state = {
         error: null,
         isLoaded: false,
-        items: []
+        items: [],
+        sortTickerAsc: true,
+        sortDailyChangeAsc: true,
+        sortBetaAsc: true
       };
     }
   
@@ -46,15 +49,15 @@ class StockComponent extends React.Component {
             <table className="table">
               <thead>
                 <tr>
-                    <th width="150px">Ticker</th>
+                    <th width="150px"><label onClick={() => this.orderTicker()}>Ticker</label></th>
                     <th width="250px">Name</th>
                     <th width="200px">Sector</th>
                     <th width="100px">Price</th>
                     <th width="100px">PE</th>
                     <th width="100px">52Low</th>
                     <th width="100px">52High</th>
-                    <th width="100px">Beta</th>
-                    <th width="100px">Daily Change</th>
+                    <th width="100px"><label onClick={() => this.orderBeta()}>Beta</label></th>
+                    <th width="100px"><label onClick={() => this.orderDailyChange()}>Daily Change</label></th>
                 </tr>
               </thead>
               <tbody>
@@ -67,7 +70,7 @@ class StockComponent extends React.Component {
                     <td align="center">{item.metrics.pe}</td>
                     <td align="center">${item.metrics.weekLow52}</td>
                     <td align="center">${item.metrics.weekHigh52}</td>
-                    <td align="center">${item.metrics.beta}</td>
+                    <td align="center">{item.metrics.beta}</td>
                     <td align="center">{item.dailyPercentChange}%</td>
                   </tr>
                 ))}
@@ -77,6 +80,73 @@ class StockComponent extends React.Component {
         )
       }
     }
+
+    orderDailyChange() {
+      this.setState({
+        items: this.state.items.sort((a, b) => {
+
+          var asc = -1;
+          if (this.state.sortDailyChangeAsc)
+          {
+            asc = 1;
+          }
+
+          if (a.dailyPercentChange < b.dailyPercentChange) {
+            return -1 * asc;
+          }
+          if (a.dailyPercentChange > b.dailyPercentChange) {
+            return 1 * asc;
+          }
+          return 0;
+        })
+      });
+      this.setState({ sortDailyChangeAsc: !this.state.sortDailyChangeAsc });
+    }
+
+    orderTicker() {
+      this.setState({
+        items: this.state.items.sort((a, b) => {
+
+          var asc = -1;
+          if (this.state.sortTickerAsc)
+          {
+            asc = 1;
+          }
+
+          if (a.ticker < b.ticker) {
+            return -1 * asc;
+          }
+          if (a.ticker > b.ticker) {
+            return 1 * asc;
+          }
+          return 0;
+        })
+      });
+      this.setState({ sortTickerAsc: !this.state.sortTickerAsc });
+    }
+
+    orderBeta() {
+      this.setState({
+        items: this.state.items.sort((a, b) => {
+
+          var asc = -1;
+          if (this.state.sortBetaAsc)
+          {
+            asc = 1;
+          }
+
+          if (a.metrics.beta < b.metrics.beta) {
+            return -1 * asc;
+          }
+          if (a.metrics.beta > b.metrics.beta) {
+            return 1 * asc;
+          }
+          return 0;
+        })
+      });
+      this.setState({ sortBetaAsc: !this.state.sortBetaAsc });
+    }
+
   }
 
   export default StockComponent;
